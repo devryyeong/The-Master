5788a1a569f3eff3d93ce62a5435c4ef12ae802b
devryyeong <thisisme20@daum.net>
2021-09-15 14:16:41 +0900

    SearchDialog List issue

--- src/components/FindMasterPage/SearchCategory.js
+++ src/components/FindMasterPage/SearchCategory.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Button } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 
+import SearchDialog from './SearchDialog';
+
 const useStyles = makeStyles((theme)=>({
     main: { 
         padding: '50px 50px',
@@ -28,21 +30,38 @@ const useStyles = makeStyles((theme)=>({
 
 }));
 
+
 export default function SearchCategory(){
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const [selectedValue, setSelectedValue] = React.useState('');
+
+    const handleClickOpen = () => {
+        setOpen(false);
+    };
+
+    const handleClose = (value) => {
+        setOpen(false);
+        setSelectedValue(value);
+    };
+
     return (
         <div className={classes.main}>
             <Typography variant="h4" className={classes.title}>고수 찾기</Typography>
             <Typography variant="body2">마스터 - 지역, 카테고리</Typography>
 
             <Box className={classes.btnBox}>
-                <Button className={classes.btn}>지역</Button>
-                <Button className={classes.btn}>카테고리</Button>
+                <Button variant="outlined" className={classes.btn} onClick={handleClickOpen}>
+                    <Typography variant="subtitle1">지역 {selectedValue}</Typography>
+                </Button>
+                <Button variant="outlined" className={classes.btn}>카테고리</Button>
             </Box>
 
             <Box className={classes.indexBox}>
                 <Typography>nn명의 고수</Typography>
             </Box>
+
+            <SearchDialog selectedValue={selectedValue} open={open} onClose={handleClose} />
 
         </div>
     );
